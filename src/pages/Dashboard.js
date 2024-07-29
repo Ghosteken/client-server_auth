54e0168f335dd3ad498e61356eb500d942d4e299
nicholas <aigberuan6@gmail.com>
2024-07-29 23:58:15 +0100

    done

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,32 +1,71 @@
 import React from "react"
 import jwt from 'jsonwebtoken'
-import {useHistory} from 'react-router-dom'
-
+import { useHistory } from 'react-router-dom'
+import { set } from "mongoose"
 
 const Dashboard = () => {
     const history = useHistory()
+    const [quote, setQuote] = useState('')
+    const [tempQuote, setTempQuote] = useState('')
 
-    async function populateQuote(){
-        const req =  await fetch('http://localhost:1337/api/quote',{
-            headers:{
+    async function populateQuote() {
+        const req = await fetch('http://localhost:1337/api/quote', {
+             headers: {
                 'x-access-token': localStorage.getItem('token'),
             },
         })
-        const data = req.json()
-        consolel.log(data)
+
+        const data = await req.json()
+        if (data.status === 'ok') {
+            setQuote(data.quote)
+        } else {
+            alert(data.error)
+        }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         const token = localStorage.getItem('token')
-        if (token){
+        if (token) {
             const User = jwt.decode(token)
-            if (!User){
+            if (!User) {
                 localStorage.removeItem('token')
                 history.replace('/login')
             }
-        } 
-    },[])
-    return <h2>hello world</h2>
+        }
+    }, [])
+
+   async function updateQuote(event){
+        event.preventDefault()
+        const req = await fetch('http://localhost:1337/api/quote', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-access-token': localStorage.getItem('token'),
+        },
+        body: JSON.stringify({
+           quote:tempQuote, 
+        }),
+    })
+
+    const data = await req.json()
+    if (data.status === 'ok') {
+        setQuote(tempQuote)
+        setTempQuote('')
+    } else {
+        alert(data.error)
+    }
+}
+
+    return (
+        <div>
+            <h1>Your quote:{quote || 'No quote found'}</h1>
+
+            <form onSubmit={updateQuote}>
+                <input type="text" placeholder="Quote" value={tempQuote} onChange={(e) => setTempQuote(e.target.value)} />
+                <input type="submit" value="Update quote"/>
+            </form>
+        </div>
+    )
 }
 
 export default Dashboard
